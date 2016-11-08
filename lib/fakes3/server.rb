aa94e7956ae93f768a01ca72b0d20e015c68fb1c
John Hobbs <john@velvetcache.org>
2016-11-07 20:30:50 -0800

    Allow any requested headers in OPTIONS.

--- lib/fakes3/server.rb
+++ lib/fakes3/server.rb
@@ -317,11 +317,12 @@ module FakeS3
 
     def do_OPTIONS(request, response)
       super
+      requested_headers = request['Access-Control-Request-Headers'].split ','
 
       response['Access-Control-Allow-Origin']   = '*'
       response['Access-Control-Allow-Methods']  = 'PUT, POST, HEAD, GET, OPTIONS'
-      response['Access-Control-Allow-Headers']  = 'Accept, Content-Type, Authorization, Content-Length, ETag'
-      response['Access-Control-Expose-Headers'] = 'ETag'
+      response['Access-Control-Allow-Headers']  = (%w('Accept Content-Type Authorization Content-Length ETag') + requested_headers).join ', '
+      response['Access-Control-Expose-Headers'] = 'ETag'      
     end
 
     private
